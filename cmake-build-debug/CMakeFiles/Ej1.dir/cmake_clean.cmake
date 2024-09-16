a6a1b17ec12751b103e1b6575f5ae5ce112f320a
ValentinGarelli <2309846@ucc.edu.ar>
2024-09-15 22:43:41 -0300

    ejercicio 3 terminado

---
+++ cmake-build-debug/CMakeFiles/Ej1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ej1.dir/Ej1.cpp.obj"
+  "CMakeFiles/Ej1.dir/Ej1.cpp.obj.d"
+  "Ej1.exe"
+  "Ej1.exe.manifest"
+  "Ej1.lib"
+  "Ej1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ej1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Ej3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ej3.dir/Ej3.cpp.obj"
+  "CMakeFiles/Ej3.dir/Ej3.cpp.obj.d"
+  "Ej3.exe"
+  "Ej3.exe.manifest"
+  "Ej3.lib"
+  "Ej3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ej3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
